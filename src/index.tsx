e490e8a505ed48c7f5e44d88db772beeb163dc35
Manish Kumar <manishprivet@protonmail.com>
2021-05-02 15:44:13 +0530

    Pruned Boilerplate and Added Prettier

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import ReactDOM from 'react-dom';
 import './index.scss';
-import App from './App';
+import App from './containers/App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
