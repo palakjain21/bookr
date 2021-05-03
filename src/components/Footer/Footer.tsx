ba072b052e023ac1cac7a5f5b6d667e5ab69b2cf
sejaljain123 <sejaljain2043@gmail.com>
2021-05-03 13:13:48 +0530

    Header and footer

---
+++ src/components/Footer/Footer.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Footer.scss'
+
+function Footer() {
+    return (
+        <div className="footer">
+            <p>Made with love by github</p>
+        
+     </div>
+    )
+}
+
+export default Footer
---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.scss'
+
+function Header() {
+    return (
+        <div className="header">
+            <nav className="register ">Register</nav>
+            <nav className="signin ">SIGN IN</nav>
+            
+        </div>
+    )
+}
+
+export default Header
