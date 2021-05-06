3a542fb8dbebe6e65c8558743ae718789ec4b66b
sejaljain123 <sejaljain2043@gmail.com>
2021-05-06 17:11:09 +0530

    resolved conflicts

--- src/components/HomePage/HomePage.tsx
+++ src/components/HomePage/HomePage.tsx
@@ -1,9 +1,16 @@
 import Hero from './Hero/Hero';
+import Register from '../HomePage/Forms/Register/Register';
+import Signin from '../HomePage/Forms/Signin/Signin';
+import styles from './Homepage.module.scss';
 
 function HomePage() {
   return (
     <>
       <Hero />
+      <div className={styles.Forms}>
+        <Signin />
+        <Register />
+      </div>
     </>
   );
 }
--- src/containers/App.tsx
+++ src/containers/App.tsx
@@ -2,11 +2,7 @@ import './App.scss';
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
 
-import Register from '../components/Forms/Register/Register';
-import Signin from '../components/Forms/Signin/Signin';
-
 import HomePage from '../components/HomePage/HomePage';
-
 
 function App() {
   return (
@@ -14,10 +10,6 @@ function App() {
       <Header />
       <HomePage />
       <Footer />
-      <div className="Forms">
-        <Signin />
-        <Register />
-      </div>
     </div>
   );
 }
